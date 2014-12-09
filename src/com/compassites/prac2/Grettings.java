cf3a2ee8dc801309f8e913455ad018dd16d849af
Jinali1 <jain.jinali71@gmail.com>
2014-12-09 16:38:06 +0530

    My Third Commit

--- src/com/compassites/prac2/Grettings.java
+++ src/com/compassites/prac2/Grettings.java
@@ -5,7 +5,7 @@ public class Grettings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hi");
+		System.out.println("Hi! How are you?");
 		Hello h = new Hello();
 		h.displayHello();
 		System.out.println("Welcome");
